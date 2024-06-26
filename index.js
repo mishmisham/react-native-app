82cee959f5f8edfeaf0579bb75bdf2f6f84c7217
Михаил <postformishamail@gmail.com>
2024-06-26 06:47:50 +0300

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 /**
  * @format
  */
-
-import {AppRegistry} from 'react-native';
+import { createRoot } from 'react-dom/client';
+import {AppRegistry, Platform} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+if (Platform.OS === 'web') {
+    const root = createRoot(document.getElementById('root'));
+    root.render(<App />);
+  }
